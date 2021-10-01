39a01eb4e1f4dec3e47e15555569fe224d0d1f13
Sam Jeeves <sam.jeeves@live.com>
2021-10-01 10:57:49 +0200

    first commit

---
+++ Source/StreamChatSample/StreamChatSampleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StreamChatSampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STREAMCHATSAMPLE_API AStreamChatSampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
