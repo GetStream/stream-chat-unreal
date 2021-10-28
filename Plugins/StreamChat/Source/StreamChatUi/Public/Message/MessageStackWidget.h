1a5e4d552291661c08c6494db8cc7b00686e158d
Sam Jeeves <jeevcat@users.noreply.github.com>
2021-10-28 14:53:26 +0200

    Rename directory

---
+++ Plugins/StreamChat/Source/StreamChatUi/Public/Message/MessageStackWidget.h
@@ -0,0 +1,19 @@
+﻿// Copyright Stream.IO, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Blueprint/UserWidget.h"
+#include "CoreMinimal.h"
+
+#include "MessageStackWidget.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class STREAMCHATUI_API UMessageStackWidget final : public UUserWidget
+{
+    GENERATED_BODY()
+
+public:
+};
---
+++ Plugins/StreamChat/Source/StreamChatUi/Public/Message/TextBubbleWidget.h
@@ -0,0 +1,19 @@
+﻿// Copyright Stream.IO, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Blueprint/UserWidget.h"
+#include "CoreMinimal.h"
+
+#include "TextBubbleWidget.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class STREAMCHATUI_API UTextBubbleWidget final : public UUserWidget
+{
+    GENERATED_BODY()
+
+public:
+};
