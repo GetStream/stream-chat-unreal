402c7c3792348d5fe9d2f80225826281b90eb339
Sam Jeeves <jeevcat@users.noreply.github.com>
2021-10-29 19:27:54 +0200

    Fix clang compiler issue

--- Plugins/StreamChat/Source/Backend/StreamChatDto/Public/Response/Message/MessageTypeDto.h
+++ Plugins/StreamChat/Source/Backend/StreamChatDto/Public/Response/Message/MessageTypeDto.h
@@ -2,10 +2,10 @@
 
 #include "CoreMinimal.h"
 
-UENUM()
 /**
  * https://getstream.io/chat/docs/other-rest/message_format/#message-types
  */
+UENUM()
 enum class EMessageTypeDto : uint8
 {
     Regular,
