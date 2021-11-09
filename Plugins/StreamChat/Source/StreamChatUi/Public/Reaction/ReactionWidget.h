0ec3a595d741216a557bf3dc63c42873b99b2cd9
Sam Jeeves <jeevcat@users.noreply.github.com>
2021-11-10 06:39:31 +1100

    Reaction widget

---
+++ Plugins/StreamChat/Source/StreamChatUi/Public/Reaction/ReactionWidget.h
@@ -0,0 +1,45 @@
+// Copyright Stream.IO, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Components/Image.h"
+#include "CoreMinimal.h"
+#include "Message/MessageSide.h"
+#include "StreamUserWidget.h"
+
+#include "ReactionWidget.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class STREAMCHATUI_API UReactionWidget : public UStreamUserWidget
+{
+    GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintCallable, Category = "Stream Chat")
+    void Setup(const FName& InReactionType, EMessageSide InSide);
+
+protected:
+    UPROPERTY(meta = (BindWidget))
+    UImage* Image;
+
+    UPROPERTY(EditAnywhere, Category = Defaults)
+    TMap<FName, UTexture2D*> Textures;
+
+    UPROPERTY(EditAnywhere, Category = Defaults)
+    bool bShouldTint = false;
+
+    UPROPERTY(EditAnywhere, Category = Defaults)
+    TMap<EMessageSide, FLinearColor> Colors;
+
+private:
+    virtual void OnSetup() override;
+
+    UPROPERTY(EditAnywhere, Category = Setup)
+    FName ReactionType;
+
+    UPROPERTY(EditAnywhere, Category = Setup)
+    EMessageSide Side;
+};
