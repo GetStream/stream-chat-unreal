794b75d1629b3e3523aa6698a4689151836ced89
Sam Jeeves <jeevcat@users.noreply.github.com>
2021-11-10 08:16:47 +1100

    Bottom reaction widget

---
+++ Plugins/StreamChat/Source/StreamChatUi/Public/Reaction/BottomReactionWidget.h
@@ -0,0 +1,40 @@
+// Copyright Stream.IO, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Components/TextBlock.h"
+#include "CoreMinimal.h"
+#include "Message/MessageSide.h"
+#include "Reaction/ReactionGroup.h"
+#include "ReactionWidget.h"
+#include "StreamUserWidget.h"
+
+#include "BottomReactionWidget.generated.h"
+
+/**
+ * https://www.figma.com/file/oeHiSMDoutuVbJ9WiVyhcG/Chat-Design-Kit-1.5-Android---Work-in-Progress?node-id=17089%3A113943
+ */
+UCLASS()
+class STREAMCHATUI_API UBottomReactionWidget final : public UStreamUserWidget
+{
+    GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintCallable, Category = "Stream Chat")
+    void Setup(const FReactionGroup& InReactionGroup);
+
+protected:
+    UPROPERTY(meta = (BindWidget))
+    UReactionWidget* ReactionIcon;
+
+    UPROPERTY(meta = (BindWidget))
+    UTextBlock* ReactionCountTextBlock;
+
+private:
+    virtual void OnSetup() override;
+
+    EMessageSide GetSide() const;
+
+    UPROPERTY(EditAnywhere, Category = Setup)
+    FReactionGroup ReactionGroup;
+};
