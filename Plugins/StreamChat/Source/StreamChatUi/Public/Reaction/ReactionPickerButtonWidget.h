e60055a159f463fe7aec1036fc9660e97bffc77b
Sam Jeeves <jeevcat@users.noreply.github.com>
2021-11-11 13:59:06 +1100

    Reaction picker and button

---
+++ Plugins/StreamChat/Source/StreamChatUi/Public/Reaction/ReactionPickerButtonWidget.h
@@ -0,0 +1,49 @@
+﻿// Copyright Stream.IO, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Components/Button.h"
+#include "CoreMinimal.h"
+#include "ReactionIconWidget.h"
+#include "StreamUserWidget.h"
+
+#include "ReactionPickerButtonWidget.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class STREAMCHATUI_API UReactionPickerButtonWidget final : public UStreamUserWidget
+{
+    GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintCallable, Category = "Stream Chat")
+    void Setup(const FName& InReactionType, EMessageSide InSide);
+
+    DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FReactionPickerButtonClicked, const FName&, ReactionType);
+    UPROPERTY(BlueprintAssignable)
+    FReactionPickerButtonClicked OnReactionPickerButtonClicked;
+
+    DECLARE_MULTICAST_DELEGATE_OneParam(FReactionPickerButtonClickedNative, const FName& /* ReactionType */);
+    FReactionPickerButtonClickedNative OnReactionPickerButtonClickedNative;
+
+protected:
+    UPROPERTY(meta = (BindWidget))
+    UButton* Button;
+
+    UPROPERTY(meta = (BindWidget))
+    UReactionIconWidget* Icon;
+
+private:
+    virtual void OnSetup() override;
+
+    UFUNCTION()
+    void OnButtonClicked();
+
+    UPROPERTY(EditAnywhere, Category = Setup)
+    FName ReactionType;
+
+    UPROPERTY(EditAnywhere, Category = Setup)
+    EMessageSide Side;
+};
---
+++ Plugins/StreamChat/Source/StreamChatUi/Public/Reaction/ReactionPickerWidget.h
@@ -0,0 +1,37 @@
+﻿// Copyright Stream.IO, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "Channel/Message.h"
+#include "CoreMinimal.h"
+#include "ReactionPickerButtonWidget.h"
+#include "StreamUserWidget.h"
+
+#include "ReactionPickerWidget.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class STREAMCHATUI_API UReactionPickerWidget final : public UStreamUserWidget
+{
+    GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintCallable, Category = "Stream Chat")
+    void Setup(const FMessage& InMessage);
+
+protected:
+    UPROPERTY(meta = (BindWidget))
+    UPanelWidget* ButtonsPanel;
+
+    UPROPERTY(EditAnywhere, NoClear, Category = Defaults)
+    TSubclassOf<UReactionPickerButtonWidget> ReactionPickerButtonWidgetClass =
+        UReactionPickerButtonWidget::StaticClass();
+
+private:
+    virtual void OnSetup() override;
+
+    UPROPERTY(EditAnywhere, Category = Setup)
+    FMessage Message;
+};
