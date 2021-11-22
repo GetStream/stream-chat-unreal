0c8a43e56facf1bbbde709ba0a00d7f704fe75a8
Sam Jeeves <jeevcat@users.noreply.github.com>
2021-11-22 11:02:10 +0100

    Add fading message

---
+++ Plugins/StreamChat/Source/StreamChatUi/Private/Message/FadingMessageWidget.cpp
@@ -0,0 +1,60 @@
+#include "Message/FadingMessageWidget.h"
+
+#include "User/User.h"
+
+void UFadingMessageWidget::Setup(const FMessage& InMessage, FTimespan InLifetime)
+{
+    Message = InMessage;
+    if (InLifetime != FTimespan{0})
+    {
+        Lifetime = InLifetime;
+    }
+
+    Super::Setup();
+}
+
+void UFadingMessageWidget::OnSetup()
+{
+    if (TextBlock)
+    {
+        if (Message.User.IsValid() && !Message.User->Name.IsEmpty())
+        {
+            TextBlock->SetText(FText::FromString(FString::Printf(TEXT("%s: %s"), *Message.User->Name, *Message.Text)));
+        }
+        else
+        {
+            TextBlock->SetText(FText::FromString(Message.Text));
+        }
+    }
+
+    if (FadeAnimation)
+    {
+        const FTimespan MessageAge = FDateTime::UtcNow() - Message.CreatedAt;
+        const FTimespan ShowFor = Lifetime - MessageAge;
+        const double Delay = FMath::Max(0., ShowFor.GetTotalSeconds() - FadeAnimation->GetEndTime());
+        GetWorld()->GetTimerManager().SetTimer(
+            TimerHandle,
+            [WeakThis = TWeakObjectPtr<UFadingMessageWidget>(this)]
+            {
+                if (WeakThis.IsValid())
+                {
+                    WeakThis->FadeOut();
+                }
+            },
+            Delay,
+            false);
+    }
+}
+
+void UFadingMessageWidget::OnAnimationFinished_Implementation(const UWidgetAnimation* Animation)
+{
+    if (Animation == FadeAnimation)
+    {
+        RemoveFromParent();
+    }
+}
+
+void UFadingMessageWidget::FadeOut()
+{
+    PlayAnimation(FadeAnimation);
+}
---
+++ Plugins/StreamChat/Source/StreamChatUi/Public/Message/FadingMessageWidget.h
@@ -0,0 +1,47 @@
+﻿// Copyright Stream.IO, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "BubbleStackPosition.h"
+#include "Channel/Message.h"
+#include "Components/Border.h"
+#include "Components/TextBlock.h"
+#include "CoreMinimal.h"
+#include "MessageSide.h"
+#include "StreamUserWidget.h"
+
+#include "FadingMessageWidget.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class STREAMCHATUI_API UFadingMessageWidget final : public UStreamUserWidget
+{
+    GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintCallable, Category = "Stream Chat")
+    void Setup(const FMessage& InMessage, FTimespan InLifetime);
+
+protected:
+    UPROPERTY(meta = (BindWidget))
+    UTextBlock* TextBlock;
+    UPROPERTY(Transient, meta = (BindWidgetAnim))
+    UWidgetAnimation* FadeAnimation;
+
+private:
+    virtual void OnSetup() override;
+
+    virtual void OnAnimationFinished_Implementation(const UWidgetAnimation* Animation) override;
+
+    void FadeOut();
+
+    UPROPERTY(EditAnywhere, Category = Setup)
+    FMessage Message;
+
+    UPROPERTY(EditAnywhere, Category = Setup)
+    FTimespan Lifetime = FTimespan::FromSeconds(20.f);
+
+    FTimerHandle TimerHandle;
+};
