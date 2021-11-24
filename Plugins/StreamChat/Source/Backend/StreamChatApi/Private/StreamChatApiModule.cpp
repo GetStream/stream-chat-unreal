f89e7cc13ce86c2767eb26f4dbec52f0f7717ef4
Sam Jeeves <jeevcat@users.noreply.github.com>
2021-11-24 10:19:55 +0100

    Set font

--- Plugins/StreamChat/Source/Backend/StreamChatApi/Private/StreamChatApiModule.cpp
+++ Plugins/StreamChat/Source/Backend/StreamChatApi/Private/StreamChatApiModule.cpp
@@ -1,4 +1,9 @@
 // Copyright Stream.IO, Inc. All Rights Reserved.
 
+/**
+ * @defgroup StreamChatApi REST API
+ * @brief This module allows for sending requests to the Stream Chat REST API
+ */
+
 #include "Modules/ModuleManager.h"
 IMPLEMENT_GAME_MODULE(FDefaultModuleImpl, StreamChatApi);
