a9182b933ffe9137a0617716e3ad5a20802067fc
Sam Jeeves <jeevcat@users.noreply.github.com>
2021-12-10 09:41:34 +0100

    Add message list class

---
+++ Plugins/StreamChat/Source/StreamChatUi/Public/Message/MessageList.h
@@ -0,0 +1,17 @@
+// Copyright 2021 Stream.IO, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "StreamUserWidget.h"
+#include "MessageList.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STREAMCHATUI_API UMessageList : public UStreamUserWidget
+{
+	GENERATED_BODY()
+	
+};
