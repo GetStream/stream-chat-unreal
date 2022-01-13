3c2d048b087002b3c46bfd750b823355ebbd4933
Sam Jeeves <jeevcat@users.noreply.github.com>
2022-01-13 09:44:38 +0100

    Add more fields to channel id

--- Plugins/StreamChat/Source/StreamChat/Public/Channel/ChannelId.h
+++ Plugins/StreamChat/Source/StreamChat/Public/Channel/ChannelId.h
@@ -46,9 +46,76 @@ struct STREAMCHAT_API FChannelId
     UPROPERTY(BlueprintReadOnly, Category = "Stream Chat|Channel")
     FChannelConfig Config;
 
+    /// The count of the members of this channel
+    UPROPERTY()
+    int32 MemberCount;
+
     /// The members of this channel
     UPROPERTY(BlueprintReadOnly, Category = "Stream Chat|Channel")
     TArray<FMember> Members;
+
+    /// Cooldown period after sending each message
+    UPROPERTY()
+    int32 Cooldown;
+
+    /// The date of channel creation
+    UPROPERTY()
+    FDateTime CreatedAt;
+
+    /// The date of the last channel update
+    UPROPERTY()
+    FDateTime UpdatedAt;
+
+    /// The date of channel deletion
+    UPROPERTY()
+    FDateTime DeletedAt;
+
+    /// The date of the last message
+    UPROPERTY()
+    FDateTime LastMessageAt;
+
+    /// The user that created this channel
+    UPROPERTY()
+    FUserRef CreatedBy;
+
+    UPROPERTY()
+    bool bDisabled;
+
+    /// Whether this channel is frozen or not
+    UPROPERTY()
+    bool bFrozen;
+
+    /// Whether this channel is hidden by current user or not
+    UPROPERTY()
+    bool bHidden;
+
+    /// Whether this channel is muted or not
+    UPROPERTY()
+    bool bMuted;
+
+    /// Date of mute expiration
+    UPROPERTY()
+    FDateTime MuteExpiresAt;
+
+    /// Date since when the message history is accessible
+    UPROPERTY()
+    FDateTime HideMessagesBefore;
+
+    /// List of channel capabilities of the authenticated user
+    UPROPERTY()
+    TArray<FString> OwnCapabilities;
+
+    /// Whether auto translation is enabled or not
+    UPROPERTY()
+    bool bAutoTranslationEnabled;
+
+    /// Language to translate to when auto translation is active
+    UPROPERTY()
+    FString AutoTranslationLanguage;
+
+    /// The team the channel belongs to (multi-tenant only)
+    UPROPERTY()
+    FString Team;
 
     /// The human-readable name of this channel
     // Not in spec so might not be set
