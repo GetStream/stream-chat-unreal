9f1d70c41bc93434472db99123427ad66137aa41
Sam Jeeves <jeevcat@users.noreply.github.com>
2022-02-02 09:35:07 +0100

    Add DTOs for guest creation

---
+++ Plugins/StreamChat/Source/Backend/StreamChatDto/Public/Response/User/GuestResponseDto.h
@@ -0,0 +1,30 @@
+// Copyright 2021 Stream.IO, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UserObjectDto.h"
+
+#include "GuestResponseDto.generated.h"
+
+/**
+ * @brief #/components/schemas/GuestResponse
+ * @ingroup StreamChatDto
+ */
+USTRUCT()
+struct FGuestResponseDto
+{
+    GENERATED_BODY()
+
+    /// Authentication token to use for guest user
+    UPROPERTY()
+    FString AccessToken;
+
+    /// Duration of the request in human-readable format
+    UPROPERTY()
+    FString Duration;
+
+    /// Created user object
+    UPROPERTY()
+    FUserObjectDto User;
+};
