55a3b6a34368e165db27ae0645ae363dfea20722
Sam Jeeves <jeevcat@users.noreply.github.com>
2022-02-16 11:30:05 +0100

    Ordered list support

--- Plugins/StreamChat/Source/StreamChatUi/Private/Common/Html/HtmlDecorators.h
+++ Plugins/StreamChat/Source/StreamChatUi/Private/Common/Html/HtmlDecorators.h
@@ -11,27 +11,30 @@ public:
     virtual bool Supports(const FTextRunParseResults& RunInfo, const FString& Text) const override;
     virtual TSharedRef<ISlateRun> Create(
         const TSharedRef<FTextLayout>& TextLayout,
-        const FTextRunParseResults& TextRun,
+        const FTextRunParseResults& ParseResults,
         const FString& ProcessedText,
         const TSharedRef<FString>& InOutModelText,
         const ISlateStyle* Style) override;
 
 protected:
-    virtual FString GetSupportedTag() const = 0;
+    virtual FName GetSupportedTag() const = 0;
     virtual TSharedRef<ISlateRun> Create(
-        const FName& Tags,
+        const FTextRunParseResults& ParseResults,
         const FRunInfo& RunInfo,
         const FTextRange& ModelRange,
         const TSharedRef<FString>& InOutModelText,
         const ISlateStyle* Style) = 0;
+
+    const TArray<FName>& GetTags(const FString& RunName) const;
+    mutable TMap<FString, TArray<FName>> ParsedTagsCache;
 };
 
 class FHyperlinkHtmlDecorator final : public FHtmlDecorator
 {
 protected:
-    virtual FString GetSupportedTag() const override;
+    virtual FName GetSupportedTag() const override;
     virtual TSharedRef<ISlateRun> Create(
-        const FName& Tags,
+        const FTextRunParseResults& ParseResults,
         const FRunInfo& RunInfo,
         const FTextRange& ModelRange,
         const TSharedRef<FString>& InOutModelText,
@@ -41,11 +44,14 @@ protected:
 class FListItemHtmlDecorator final : public FHtmlDecorator
 {
 protected:
-    virtual FString GetSupportedTag() const override;
+    virtual FName GetSupportedTag() const override;
     virtual TSharedRef<ISlateRun> Create(
-        const FName& Tags,
+        const FTextRunParseResults& ParseResults,
         const FRunInfo& RunInfo,
         const FTextRange& ModelRange,
         const TSharedRef<FString>& InOutModelText,
         const ISlateStyle* Style) override;
+
+    int32 LastChar = INDEX_NONE;
+    uint32 Order = 0;
 };
