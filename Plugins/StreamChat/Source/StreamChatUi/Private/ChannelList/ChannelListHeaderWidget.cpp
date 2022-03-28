575ed5f844de965061ddf0971fecb6644cae711d
Sam Jeeves <jeevcat@users.noreply.github.com>
2022-03-28 18:34:08 +0200

    Show New Chat page when clicking channel status

--- Plugins/StreamChat/Source/StreamChatUi/Private/ChannelList/ChannelListHeaderWidget.cpp
+++ Plugins/StreamChat/Source/StreamChatUi/Private/ChannelList/ChannelListHeaderWidget.cpp
@@ -2,11 +2,22 @@
 
 #include "ChannelList/ChannelListHeaderWidget.h"
 
+#include "Context/ClientContextWidget.h"
 #include "ThemeDataAsset.h"
 
 UChannelListHeaderWidget::UChannelListHeaderWidget()
 {
+    bAutoSetup = true;
     bWantsTheme = true;
+    bWantsClient = true;
+}
+
+void UChannelListHeaderWidget::OnSetup()
+{
+    if (Button)
+    {
+        Button->OnClicked.AddDynamic(this, &UChannelListHeaderWidget::OnButtonClick);
+    }
 }
 
 void UChannelListHeaderWidget::OnTheme()
@@ -14,5 +25,13 @@ void UChannelListHeaderWidget::OnTheme()
     if (Icon)
     {
         Icon->SetColorAndOpacity(Theme->GetPaletteColor(Theme->ChannelListHeaderIconColor));
+    }
+}
+
+void UChannelListHeaderWidget::OnButtonClick()
+{
+    if (const UClientContextWidget* Context = UClientContextWidget::Get(this))
+    {
+        Context->OnNewChat.Broadcast();
     }
 }
