269bdc0d088a5b5b5721529e83e1e69fad2d6b40
Sam Jeeves <jeevcat@users.noreply.github.com>
2022-04-27 15:29:34 +0200

    Add UpdateChannelRequest DTO

---
+++ Plugins/StreamChat/Source/Backend/StreamChatDto/Public/Request/Channel/ChannelMemberRequestDto.h
@@ -0,0 +1,64 @@
+// Copyright 2022 Stream.IO, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Request/User/UserObjectRequestDto.h"
+
+#include "ChannelMemberRequestDto.generated.h"
+
+/**
+ * @brief #/components/schemas/ChannelMemberRequest
+ * @ingroup StreamChatDto
+ */
+USTRUCT()
+struct FChannelMemberRequestDto
+{
+    GENERATED_BODY()
+
+    /// Expiration date of the ban
+    UPROPERTY()
+    FDateTime BanExpires = FDateTime{0};
+
+    /// Whether member is banned in this channel or not
+    UPROPERTY()
+    bool bBanned = false;
+
+    /// Role of the member in the channel
+    UPROPERTY()
+    FString ChannelRole;
+
+    /// Date/time of creation
+    UPROPERTY()
+    FDateTime CreatedAt = FDateTime{0};
+
+    /// Date when invite was accepted
+    UPROPERTY()
+    FDateTime InviteAcceptedAt = FDateTime{0};
+
+    /// Date when invite was rejected
+    UPROPERTY()
+    FDateTime InviteRejectedAt = FDateTime{0};
+
+    /// Whether member was invited or not
+    UPROPERTY()
+    bool bInvited = false;
+
+    /// Whether member is channel moderator or not
+    UPROPERTY()
+    bool bIsModerator = false;
+
+    /// Whether member is shadow banned in this channel or not
+    UPROPERTY()
+    bool bShadowBanned = false;
+
+    /// Date/time of the last update
+    UPROPERTY()
+    FDateTime UpdatedAt = FDateTime{0};
+
+    UPROPERTY()
+    FUserObjectRequestDto User;
+
+    UPROPERTY()
+    FString UserId;
+};
