973b12b4a01895af62aa62e6c12826873a121aee
Sam Jeeves <jeevcat@users.noreply.github.com>
2022-05-04 14:45:49 +0200

    Mute Channel Request DTO

---
+++ Plugins/StreamChat/Source/Backend/StreamChatDto/Public/Request/Channel/MuteChannelRequestDto.h
@@ -0,0 +1,25 @@
+// Copyright 2022 Stream.IO, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "MuteChannelRequestDto.generated.h"
+
+/**
+ * @brief #/components/schemas/MuteChannelRequest
+ * @ingroup StreamChatDto
+ */
+USTRUCT()
+struct FMuteChannelRequestDto
+{
+    GENERATED_BODY()
+
+    /// Channel CIDs to mute (if multiple channels)
+    UPROPERTY()
+    TArray<FString> ChannelCids;
+
+    /// Duration of mute in milliseconds
+    UPROPERTY()
+    uint32 Duration = TNumericLimits<uint32>::Max();
+};
