df28bff3ef21eb8e92d05e51ea509a6b5d1aa313
Sam Jeeves <jeevcat@users.noreply.github.com>
2022-06-13 15:55:51 +0200

    'Channels' section

---
+++ Source/DocsSamples/Private/Channels/DeletingChannels.cpp
@@ -0,0 +1,15 @@
+﻿// Copyright 2022 Stream.IO, Inc. All Rights Reserved.
+
+#include "Channel/ChatChannel.h"
+#include "CoreMinimal.h"
+
+namespace
+{
+UChatChannel* Channel = nullptr;
+
+// https://getstream.io/chat/docs/unreal/channel_delete/?language=unreal#deleting-a-channel
+void DeletingAChannel()
+{
+    Channel->Delete();
+}
+}    // namespace
---
+++ Source/DocsSamples/Private/Channels/ThrottlingAndSlowMode.cpp
@@ -0,0 +1,13 @@
+﻿// Copyright 2022 Stream.IO, Inc. All Rights Reserved.
+
+#include "CoreMinimal.h"
+
+namespace
+{
+
+// https://getstream.io/chat/docs/unreal/slow_mode/?language=unreal#channel-slow-mode
+void ChannelSlowMode()
+{
+    // Not yet supported in the Unreal SDK
+}
+}    // namespace
