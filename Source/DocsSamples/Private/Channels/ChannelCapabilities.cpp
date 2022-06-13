5be76a8894c59e73faead7c084eba8c7b697559a
Sam Jeeves <jeevcat@users.noreply.github.com>
2022-06-13 13:51:43 +0200

    Channel capabilities + Invites

---
+++ Source/DocsSamples/Private/Channels/ChannelCapabilities.cpp
@@ -0,0 +1,13 @@
+﻿// Copyright 2022 Stream.IO, Inc. All Rights Reserved.
+
+#include "CoreMinimal.h"
+
+namespace
+{
+
+// https://getstream.io/chat/docs/unreal/channel_capabilities/?language=unreal
+void ChannelCapabilities()
+{
+    // Not yet supported in the Unreal SDK
+}
+}    // namespace
