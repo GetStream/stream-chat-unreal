d6e67ca33aba56a3d46bbd5578a80cb0ed69647c
Sam Jeeves <jeevcat@users.noreply.github.com>
2022-06-13 13:51:43 +0200

    Channel capabilities + Invites

---
+++ Source/DocsSamples/Private/Channels/Invites.cpp
@@ -0,0 +1,45 @@
+﻿// Copyright 2022 Stream.IO, Inc. All Rights Reserved.
+
+#include "CoreMinimal.h"
+#include "StreamChatClientComponent.h"
+
+namespace
+{
+UStreamChatClientComponent* Client;
+
+// https://getstream.io/chat/docs/unreal/channel_invites/?language=unreal#inviting-users
+void InvitingUsers()
+{
+    // Not yet supported in the Unreal SDK
+}
+
+// https://getstream.io/chat/docs/unreal/channel_invites/?language=unreal#accepting-an-invite
+void AcceptingAnInvite()
+{
+    // Not yet supported in the Unreal SDK
+}
+
+// https://getstream.io/chat/docs/unreal/channel_invites/?language=unreal#rejecting-an-invite
+void RejectingAnInvite()
+{
+    // Not yet supported in the Unreal SDK
+}
+
+// https://getstream.io/chat/docs/unreal/channel_invites/?language=unreal#query-for-accepted-invites
+void QueryForAcceptedInvites()
+{
+    Client->QueryChannels(FFilter::Equal(TEXT("invite"), TEXT("accepted")));
+}
+
+// https://getstream.io/chat/docs/unreal/channel_invites/?language=unreal#query-for-rejected-invites
+void QueryForRejectedInvites()
+{
+    Client->QueryChannels(FFilter::Equal(TEXT("invite"), TEXT("rejected")));
+}
+
+// https://getstream.io/chat/docs/unreal/channel_invites/?language=unreal#query-for-pending-invites
+void QueryForPendingInvites()
+{
+    Client->QueryChannels(FFilter::Equal(TEXT("invite"), TEXT("pending")));
+}
+}    // namespace
