99075647e3a16a0742cbd0b2ef5a3a130c7191a2
jeevcat <jeevcat@users.noreply.github.com>
2022-08-30 14:02:43 +0000

    deploy: 76fa4731fe073a881b398cb1b0be3c837eef126d

--- search/functions_c.js
+++ search/functions_c.js
@@ -1,13 +1,16 @@
 var searchData=
 [
-  ['markallread_0',['MarkAllRead',['../class_u_stream_chat_client_component.html#a3c2012df6e551396792ffb92b3775221',1,'UStreamChatClientComponent']]],
-  ['markchannelread_1',['MarkChannelRead',['../class_f_chat_api.html#ac79a995b3863ea72fd2a08f699e89cbe',1,'FChatApi']]],
-  ['markchannelsread_2',['MarkChannelsRead',['../class_f_chat_api.html#a32d254330486c4a731e0173a6dd9015d',1,'FChatApi']]],
-  ['markread_3',['MarkRead',['../struct_f_channel_state.html#a0930d73fd962d4e4c109fb41cdef4624',1,'FChannelState::MarkRead()'],['../class_u_chat_channel.html#aebd79c869ab9e0a304e7df8b48d42bfb',1,'UChatChannel::MarkRead(const TOptional&lt; FMessage &gt; &amp;Message={})'],['../class_u_chat_channel.html#a4c14c007dc5cae06c50b45fd747bf13b',1,'UChatChannel::MarkRead(const FMessage &amp;Message)']]],
-  ['markreadlocal_4',['MarkReadLocal',['../class_u_chat_channel.html#aaf72201671a13f0a169843c793089de4',1,'UChatChannel']]],
-  ['merge_5',['Merge',['../struct_f_channel_properties.html#aba111098a5e76f49b99d49fcf69aaf43',1,'FChannelProperties::Merge(const FChannelResponseDto &amp;, UUserManager *)'],['../struct_f_channel_properties.html#a1ac8843f29a5125ea7e51cc7a3046005',1,'FChannelProperties::Merge(const FChannelResponseDto &amp;Dto, const TArray&lt; FChannelMemberDto &gt; &amp;InMembers, UUserManager *)']]],
-  ['mutebp_6',['MuteBP',['../class_u_chat_channel.html#ab5741335e2c975fb9eafc6a59b94e111',1,'UChatChannel']]],
-  ['mutechannels_7',['MuteChannels',['../class_f_chat_api.html#a6c54f41a3abbab8df8488a1de2aa12db',1,'FChatApi']]],
-  ['muteuserbp_8',['MuteUserBP',['../class_u_stream_chat_client_component.html#a47b35b743dca85a1e73fceaabdcada3d',1,'UStreamChatClientComponent']]],
-  ['muteusers_9',['MuteUsers',['../class_f_chat_api.html#ab811c2b71259baffea358f8e1385acca',1,'FChatApi']]]
+  ['makechannelpropertiesid_0',['MakeChannelPropertiesId',['../class_u_channel_properties_blueprint_library.html#a509e9f3d0135932af1456fc09a094244',1,'UChannelPropertiesBlueprintLibrary']]],
+  ['makechannelpropertiesuserids_1',['MakeChannelPropertiesUserIds',['../class_u_channel_properties_blueprint_library.html#a855ff82ccecbe29f035ec3e5c82838b8',1,'UChannelPropertiesBlueprintLibrary']]],
+  ['makechannelpropertiesusers_2',['MakeChannelPropertiesUsers',['../class_u_channel_properties_blueprint_library.html#a1440f5b4ca7d81571d37b3aef1c75e62',1,'UChannelPropertiesBlueprintLibrary']]],
+  ['markallread_3',['MarkAllRead',['../class_u_stream_chat_client_component.html#a3c2012df6e551396792ffb92b3775221',1,'UStreamChatClientComponent']]],
+  ['markchannelread_4',['MarkChannelRead',['../class_f_chat_api.html#ac79a995b3863ea72fd2a08f699e89cbe',1,'FChatApi']]],
+  ['markchannelsread_5',['MarkChannelsRead',['../class_f_chat_api.html#a32d254330486c4a731e0173a6dd9015d',1,'FChatApi']]],
+  ['markread_6',['MarkRead',['../struct_f_channel_state.html#a0930d73fd962d4e4c109fb41cdef4624',1,'FChannelState::MarkRead()'],['../class_u_chat_channel.html#aebd79c869ab9e0a304e7df8b48d42bfb',1,'UChatChannel::MarkRead(const TOptional&lt; FMessage &gt; &amp;Message={})'],['../class_u_chat_channel.html#a4c14c007dc5cae06c50b45fd747bf13b',1,'UChatChannel::MarkRead(const FMessage &amp;Message)']]],
+  ['markreadlocal_7',['MarkReadLocal',['../class_u_chat_channel.html#aaf72201671a13f0a169843c793089de4',1,'UChatChannel']]],
+  ['merge_8',['Merge',['../struct_f_channel_properties.html#aba111098a5e76f49b99d49fcf69aaf43',1,'FChannelProperties::Merge(const FChannelResponseDto &amp;, UUserManager *)'],['../struct_f_channel_properties.html#a1ac8843f29a5125ea7e51cc7a3046005',1,'FChannelProperties::Merge(const FChannelResponseDto &amp;Dto, const TArray&lt; FChannelMemberDto &gt; &amp;InMembers, UUserManager *)']]],
+  ['mutebp_9',['MuteBP',['../class_u_chat_channel.html#ab5741335e2c975fb9eafc6a59b94e111',1,'UChatChannel']]],
+  ['mutechannels_10',['MuteChannels',['../class_f_chat_api.html#a6c54f41a3abbab8df8488a1de2aa12db',1,'FChatApi']]],
+  ['muteuserbp_11',['MuteUserBP',['../class_u_stream_chat_client_component.html#a47b35b743dca85a1e73fceaabdcada3d',1,'UStreamChatClientComponent']]],
+  ['muteusers_12',['MuteUsers',['../class_f_chat_api.html#ab811c2b71259baffea358f8e1385acca',1,'FChatApi']]]
 ];
